e60348f846dad24f35346184efd6d4b1ecada431
ericagaffud <ericadurangaffud@gmail.com>
2024-11-14 09:28:12 +0800

    header click events

--- script/header.js
+++ script/header.js
@@ -6,9 +6,12 @@ $(document).ready(function(){
         $('.header-menu').addClass('ascending');
     
         // Hide the menu after the animation completes
-        setTimeout(function() {
-            $('.header-menu').hide();
-            $('.header-menu').removeClass('ascending');
-        }, 1000);
-    })
+        // setTimeout(function() {
+        //     $('.header-menu').hide();
+        //     $('.header-menu').removeClass('ascending');
+        // }, 1000);
+
+        var page = $(this).data('page');
+        window.location.href = page == '' ? 'index.html' : 'pages/' + page + '.html';
+    });    
 })
