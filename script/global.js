98cbee6733404fa4df3fe4f96880ffdacccb70dd
ericagaffud <ericadurangaffud@gmail.com>
2024-11-20 13:37:10 +0800

    page header implementation

--- script/global.js
+++ script/global.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
     $('.header').load('../../components/header.html');
     $('.header-menu').load('../../components/header-menu.html');
+    $('.page-header').load('../../components/page-header.html');
 })
