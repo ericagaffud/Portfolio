6861767401d6f2d3e25ab1cf46027ffa04617973
ericagaffud <ericadurangaffud@gmail.com>
2024-12-02 17:11:21 +0800

    header page menu

--- script/global.js
+++ script/global.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
     $('.header').load('./components/header.html');
     $('.header-menu').load('./components/header-menu.html');
+
+    $('.header-page').load('../components/header.html');
+    $('.header-page-menu').load('../components/header-menu.html');
+
     $('.page-header').load('../components/page-header.html');
 })
