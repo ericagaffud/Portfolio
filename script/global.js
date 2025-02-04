5db9fd025b4937b07be49c4a797a54b329df7497
Erica Gaffud <ericadurangaffud@gmail.com>
2025-02-04 21:11:48 +0800

    home version 2

--- script/global.js
+++ script/global.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-    $('.header').load('./components/header.html');
-    $('.header-menu').load('./components/header-menu.html');
+// $(document).ready(function(){
+//     $('.header').load('./components/header.html');
+//     $('.header-menu').load('./components/header-menu.html');
 
-    $('.header-page').load('../components/header.html');
-    $('.header-page-menu').load('../components/header-menu.html');
+//     $('.header-page').load('../components/header.html');
+//     $('.header-page-menu').load('../components/header-menu.html');
 
-    $('.page-header').load('../components/page-header.html');
-})
+//     $('.page-header').load('../components/page-header.html');
+// })
