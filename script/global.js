400d8c4ce10d353de4bebeb4d74fc3bd0c1b9f18
Erica Gaffud <ericadurangaffud@gmail.com>
2025-02-09 23:17:55 +0800

    navigation & separate header styles

--- script/global.js
+++ script/global.js
@@ -1,22 +1,42 @@
 $(document).ready(function(){
-    $('.header').load('./components/header.html');
-    $('#bodyHome').load('./components/home.html');
-    $('#bodyAbout').load('./components/about.html');
-    $('#bodyWork').load('./components/work.html');
+     /* Load Components */
+     $('.header').load('./components/header.html', function(){
+        $('.header-nav-item[data-page="Home"]').addClass('active-item');
 
-    /* Header Nav */
-    $('#headerMenu').on('click', function(){
+        $(document).on('click', '.header-nav-item', function(){
+            $('.header-nav-item').removeClass('active-item');
+            $(this).addClass('active-item');
+
+            $('.header-nav').removeClass('active-nav');
+            $('.header-overlay').css('display', 'none');
+
+            var dataPage = $(this).data('page');
+            var pageID = '#body' + dataPage;
+
+            $('.body, .body-about, .body-work').hide();
+            $(pageID).show(); 
+        });
+    });
+
+    $('#bodyHome').load('./components/home.html', function(){
+        $('#bodyAbout').load('./components/about.html', function(){
+            $('#bodyWork').load('./components/work.html', function(){
+                $('#bodyAbout, #bodyWork').hide();
+            });
+        });
+    });
+
+     /* Header Nav */
+     $(document).on('click', '#headerMenu', function(){
         $('.header-nav').addClass('active-nav');
         $('.header-overlay').css('display', 'block');
     });
 
-    $('.header-nav-close').on('click', function(){
+    $(document).on('click', '.header-nav-close', function(){
         $('.header-nav').removeClass('active-nav');
         $('.header-overlay').css('display', 'none');
-    })
+    });
 
-    /* Header Nav Ends */
-    
     // $('.header-menu').load('./components/header-menu.html');
 
     // $('.header-page').load('../components/header.html');
