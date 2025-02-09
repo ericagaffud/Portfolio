a28dd4d767529c970f45f2bce5a93d4d15ef252b
Erica Gaffud <ericadurangaffud@gmail.com>
2025-02-09 22:40:55 +0800

    pages set as component

--- script/global.js
+++ script/global.js
@@ -1,9 +1,26 @@
-// $(document).ready(function(){
-//     $('.header').load('./components/header.html');
-//     $('.header-menu').load('./components/header-menu.html');
+$(document).ready(function(){
+    $('.header').load('./components/header.html');
+    $('#bodyHome').load('./components/home.html');
+    $('#bodyAbout').load('./components/about.html');
+    $('#bodyWork').load('./components/work.html');
 
-//     $('.header-page').load('../components/header.html');
-//     $('.header-page-menu').load('../components/header-menu.html');
+    /* Header Nav */
+    $('#headerMenu').on('click', function(){
+        $('.header-nav').addClass('active-nav');
+        $('.header-overlay').css('display', 'block');
+    });
 
-//     $('.page-header').load('../components/page-header.html');
-// })
+    $('.header-nav-close').on('click', function(){
+        $('.header-nav').removeClass('active-nav');
+        $('.header-overlay').css('display', 'none');
+    })
+
+    /* Header Nav Ends */
+    
+    // $('.header-menu').load('./components/header-menu.html');
+
+    // $('.header-page').load('../components/header.html');
+    // $('.header-page-menu').load('../components/header-menu.html');
+
+    // $('.page-header').load('../components/page-header.html');
+})
